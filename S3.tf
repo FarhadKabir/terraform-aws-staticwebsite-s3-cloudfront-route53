8a9d86ccb6e0dfd9fb273f49f2189af17f276191
FarhadKabir <farhad_kabir1@yahoo.com>
2020-04-30 05:41:05 -0700

    aws s3 bucket to host our website with public permisson policy

---
+++ S3.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "static_web" {
+  bucket = var.bucket_name
+  acl = "public-read"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_upload_firmware_policy" {
+  bucket = aws_s3_bucket.static_web.id
+
+  policy = <<EOF
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AddPerm",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.bucket_name}/*"]
+      }
+  ]
+}
+EOF
+}
